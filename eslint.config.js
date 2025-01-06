8e336c76c76434671a95af7d1a57115f68f0c3d0
Tomáš Chochola <chocholatom1997@gmail.com>
2025-01-06 23:13:05 +0100

    Use CC-BY-ND-4.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,17 @@
 /**
  * @file
  * @author Tomáš Chochola <chocholatom1997@gmail.com>
- * @copyright © 2024–Present Tomáš Chochola <chocholatom1997@gmail.com>. All rights reserved.
+ * @copyright © 2025, Tomáš Chochola <chocholatom1997@gmail.com>. Some rights reserved.
  *
- * @license
+ * @license CC-BY-ND-4.0
  *
- * This software is proprietary and licensed under specific terms set by its owner.
- * Any form of access, use, or distribution requires a valid and active license.
- * For full licensing terms, refer to the LICENSE.md file accompanying this software.
- *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
  * @see {@link https://premierstacks.com} Website
  * @see {@link https://github.com/tomchochola} GitHub Personal
  * @see {@link https://github.com/premierstacks} GitHub Organization
  * @see {@link https://github.com/sponsors/tomchochola} GitHub Sponsors
  */
 
-import { ignores, node, configs } from '@premierstacks/eslint-stack';
+import { configs, ignores, node } from '@premierstacks/eslint-stack';
 
 export default [...ignores(), ...node(), ...configs()];
